f4d7c4c9a8aa005483815eb29b8cb11cb2e2c292
Olga <olyakapran@yandex.by>
2022-08-24 12:09:00 +0300

    Third commit

---
+++ Задача3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Задача3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void DayNumber()
+            {
+                Console.Write("Введите число от 1 до 7: ");
+                int x=Convert.ToInt32(Console.ReadLine());
+                if (x==7|| x==8)
+                {
+                    Console.WriteLine(x+" - Выходной");
+                }
+                else
+                {
+                    Console.WriteLine(x+" - Это не выходной");
+                }
+            }
+            DayNumber();
+        }
+    }
+}
