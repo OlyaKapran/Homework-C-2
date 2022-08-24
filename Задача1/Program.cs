b938e2991e8bb33d5c921bbabca7ecb373661e2a
Olga <olyakapran@yandex.by>
2022-08-24 12:05:28 +0300

    first commit

---
+++ Задача1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Задача1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void SecondNumber()
+            {
+            Console.Write("Введите трехзначное число ");
+            int x=Convert.ToInt32(Console.ReadLine());
+            int x1=x/10;
+            int x2=x1%10;
+            Console.WriteLine("Вторая цифра = "+x2);
+            }
+            SecondNumber();
+        }
+    }
+}
